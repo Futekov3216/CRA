a9b811fb6f25f99d21910e24356b529ae36058e4
Ivan <noodle.hell@gmail.com>
2021-12-01 19:34:32 +0200

    rest

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import styled from 'styled-components/macro'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+	<Diva>ASD</Diva> 
+    </div>
+  );
+}
+
+export default App;
+
+const Diva = styled.div`width:100%`
